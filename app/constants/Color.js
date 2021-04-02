537f89177d83aa1bd96820b265e57e325b35da6f
Rahul <rdubey@deqode.com>
2021-04-02 17:45:17 +0530

    Added utils and constant folder

---
+++ app/constants/Color.js
@@ -0,0 +1,8 @@
+export const Colors = {
+  WHITE: '#fff',
+  BLACK: '#000',
+  GREEN: '#EAEB5E',
+  SILVER: '#C0C0C0',
+  LIGHT_BLUE: '#acd4d9',
+  RED: '#FF0000',
+};
---
+++ app/localization/CommonLocalizationStrings.js
@@ -0,0 +1,8 @@
+import LocalizedStrings from 'react-native-localization';
+
+export const CommonLocalizeStrings = new LocalizedStrings({
+  // English
+  en: {
+    search: 'Search...',
+  },
+});
